c514766830b28072d1026b3080579602d3817dd1
Antonio001931 <andriamihavanaantonio@gmail.com>
2023-03-17 13:48:45 +0300

    sprint3

--- src/java/etu1931/framework/servlet/FrontServlet.java
+++ src/java/etu1931/framework/servlet/FrontServlet.java
@@ -1,8 +1,9 @@
+package etu1931.framework.servlet;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package etu1931.framework.servlet;
 
 import etu1931.framework.Mapping;
 import java.io.IOException;
@@ -14,6 +15,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import annotation.Url;
+import etu1931.framework.Mapping;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Find_annotation;
+
+
 
 /**
  *
@@ -33,7 +44,25 @@ public class FrontServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    HashMap<String,Mapping> MappinUrls;
+    HashMap<String,Mapping>  mappinUrls;
+    
+       public void init() throws ServletException{
+        mappinUrls=new HashMap<String,Mapping>();
+        List<Find_annotation.AnnotatedMethod> annotatedMethods;
+        try {
+            annotatedMethods = Find_annotation.findAnnotatedMethods();
+            for (Find_annotation.AnnotatedMethod method : annotatedMethods) {
+           // out.println("Annotated method " + method.getMethod().getName() + " in class " + method.getClassName() +" value ="+method.getValue());
+           Mapping mapping = new Mapping(method.getClassName(),method.getMethod().getName());
+           mappinUrls.put(method.getValue(), mapping);
+            }
+
+        } catch (Exception ex) {
+           
+            System.out.println(ex.getMessage());
+        }
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter out = response.getWriter();
@@ -42,16 +71,13 @@ public class FrontServlet extends HttpServlet {
         out.println("Context Path :"+request.getContextPath());
         out.println("");
         out.println("URL :"+request.getRequestURL());
-        out.println("");
- 
-        Enumeration<String> liste = request.getParameterNames();
-        while(liste.hasMoreElements()){
-            String element = liste.nextElement();
-            String[] elementValues = request.getParameterValues(element);
-            for(int i=0 ; i<elementValues.length ; i++){
-                out.println(element+" "+(i+1)+" : "+elementValues[i]);
-            }
+        out.println("");     
+         for (Map.Entry<String,Mapping> entry : mappinUrls.entrySet()) {
+            
+            out.println("KEY--> "+entry.getKey() + " ,VALUE:--> Class :" +entry.getValue().getClassName() + " , Method : " + entry.getValue().getMethod());
+
         }
+       
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -96,3 +122,4 @@ public class FrontServlet extends HttpServlet {
     }// </editor-fold>
 
 }
+
